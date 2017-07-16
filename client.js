0b8d810663afed761f188c5c41a3c58a803a155f
creightons <creightons26@gmail.com>
2017-07-15 22:31:03 -0400

    Implemented simple promise-based server-side RPC; Currently only handles successful responses

--- client.js
+++ client.js
@@ -2,10 +2,49 @@ const WebSocket = require('ws');
 
 const ws = new WebSocket('ws://localhost:8080');
 
-const registerMessage = {
-    type: 'REGISTER',
-    name: 'test-client',
+const name = process.argv[2];
+const secret = process.argv[3];
+
+const errors = [];
+if (!name) { errors.push('Error: must pass in a client name');}
+if (!secret) { errors.push('Error: must pass in a secret to share');}
+
+if (errors.length > 0) {
+    errors.forEach(err => console.log(err));
+    process.exit();
 }
+
+const registerMessage = { type: 'REGISTER', name };
     
+function handleRPCAction(message) {
+    const { id, action, args } = message;
+
+    console.log(`From server: action = ${action}, args = ${args}.`);
+    
+    const response = {
+        type: 'RPC_RESPONSE',
+        id,
+        clientName: name,
+        data: secret,
+    };
+
+    const stringifiedResponse = JSON.stringify(response);
+    ws.send(stringifiedResponse);
+}
+
 ws.on('open', () => ws.send(JSON.stringify(registerMessage)));
-ws.on('message', message => console.log(message));
+
+ws.on('message', router);
+
+function router(messageString) {
+    const message = JSON.parse(messageString);
+
+    switch(message.type) {
+        case 'RPC_ACTION':
+            handleRPCAction(message);
+            break;
+        
+        default:
+            console.log(`Random message: ${message}`);
+    }
+}
--- server.js
+++ server.js
@@ -1,27 +1,72 @@
 const WebSocket = require('ws');
-
 const wss = new WebSocket.Server({ port: 8080 });
+const socketRegistry = {};
 
-const responseCache = {};
+function getDeferred() {
+    const deferred = {};
 
-const RPC = ' REMOTE_PROCEDURE_CALL';
-let messageId = 1;
+    deferred.promise = new Promise((resolve, reject) => {
+        deferred.resolve = resolve;
+        deferred.reject = reject;
+    });
 
-function createMessage(method, args) {
-    const message = {
-        type: RPC,
-        id: messageId,
-        method,
-        args,
-    };
-
-    messageId += 1;
-
-    return message;
+    return deferred;
 }
 
+// Object to manage all RPC interactions
+const rpcManager = {
 
-const clientRegistry = {};
+    // Stores deferred objects generated when an RPC action is sent to a client
+    rpcRecords: {},
+
+    // Tracks all RPC messages sent to clients
+    messageId: 1,
+
+    // Makes keys for stored RPC deferred objects
+    getKey(id, client) {
+       return  `${id}::${client}`;
+    },
+
+    // Sends an RPC action and returns a Promise
+    sendRPC(clientName, action, args) {
+        const rpcMessage = {
+            id: this.messageId,
+            type: 'RPC_ACTION',
+            target: clientName,
+            action,
+            args,
+        };
+        
+        this.messageId += 1;
+
+        const key = this.getKey(rpcMessage.id, clientName);
+
+        // Uses a deferred object so that RPC actions can use promise chains
+        const deferred = getDeferred();
+
+        this.rpcRecords[key] = deferred;
+
+        const stringifiedMessage = JSON.stringify(rpcMessage);
+
+        socketRegistry[clientName].send(stringifiedMessage);
+
+        return deferred.promise;
+    },
+
+    // Process RPC response and trigger Promises
+    handleRPCResponse(response) {
+        const { id, clientName, data } = response;
+
+        const key = this.getKey(id, clientName);
+
+        const deferred = this.rpcRecords[key];
+
+        // Release the deferred object to prevent memory leaks
+        delete this.rpcRecords[key];
+
+        deferred.resolve(data);
+    }
+};
 
 wss.on('connection', ws => {
     ws.on('message', message => router(ws, message));
@@ -32,13 +77,20 @@ function router(ws, messageString) {
 
     switch(message.type) {
         case 'REGISTER':
-            clientRegistry[message.name] = ws;
+            socketRegistry[message.name] = ws;
+            break;
+
+        case 'RPC_RESPONSE':
+            rpcManager.handleRPCResponse(message);
+            break;
     }
 }
 
 
 const intervalId = setInterval(() => {
-    for (let clientName in clientRegistry) {
-        clientRegistry[clientName].send('test-message');
+    for (let clientName in socketRegistry) {
+        rpcManager.sendRPC(clientName, 'test-rpc', [ 'some', 'random', 'data'])
+            .then(res => console.log(`RPC response from '${clientName}': ${res}`))
+            .catch(err => console.log(`RPC error = ${err}`));
     }
 }, 5000);
